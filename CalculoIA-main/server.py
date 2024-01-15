051c064e962ef561dcac26f52bbc8dcf9eb044fa
juanpperezj1 <juanpperezj1@gmail.com>
2024-01-14 22:01:08 -0600

    line 38

--- CalculoIA-main/server.py
+++ CalculoIA-main/server.py
@@ -33,8 +33,6 @@ bard = BardCookies(cookie_dict=cookie_dict)
 
 # print(bard.get_answer(str(message))['content'])
 
-st.markdown("""<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous>""")
-
 
 st.title("CalculoIA tutor")
 
